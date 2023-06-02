167da6c078c2c594bce252cc80ede208cfe6e9bb
Diana Regina <devdianaregina@gmail.com>
2023-06-02 14:29:14 -0300

    atualizacao css

--- catalogo-filme/src/app/catalogo-filme/catalogo-filme.module.ts
+++ catalogo-filme/src/app/catalogo-filme/catalogo-filme.module.ts
@@ -4,10 +4,6 @@ import { CatalogoFilmeComponent } from './catalogo-filme.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatLabel } from '@angular/material/form-field';
-import { NgxPaginationModule } from 'ngx-pagination';
-
-
 
 @NgModule({
   declarations: [CatalogoFilmeComponent],
@@ -17,8 +13,7 @@ import { NgxPaginationModule } from 'ngx-pagination';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    NgxPaginationModule
   ],
-  exports: [CatalogoFilmeComponent]
+  exports: [CatalogoFilmeComponent],
 })
-export class CatalogoFilmeModule { }
+export class CatalogoFilmeModule {}
